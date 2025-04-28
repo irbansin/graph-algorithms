3a9da017b1ea371f517abdcae61d8cc818c7bff5
Anirban Sinha <97022463+irbansin@users.noreply.github.com>
2025-04-28 17:46:23 +0530

    Create 3-dijikstras.js

---
+++ 3-dijikstras.js
@@ -0,0 +1,50 @@
+function dijikstras(graph, source) {
+    /* 
+      1. initialise dist
+      2. extract the node with least weight
+      3. visit all unvisited node and update the minimum weight from source
+    */
+    let visited = new Set()
+    let dist = {}
+    // initializing dist
+    for(let node in graph){
+      dist[node] = Infinity
+    }
+    dist[source] = 0
+  
+    let pq = [[0,source]]
+
+    while(pq.length) {
+      pq.sort((a,b)=>a[0]-b[0])
+
+      //extract
+      let [weight,node] = pq.shift()
+      
+      if(visited.has(node)) continue
+      visited.add(node)
+
+      //visit unvisited nodes
+      for(let [neighbour, neighbourWeight] of graph[node]) {
+        let currentWeight = neighbourWeight + weight
+
+        //change minimum distance
+        if(currentWeight < dist[neighbour]) {
+          dist[neighbour] = currentWeight
+          pq.push([currentWeight, neighbour])
+        }
+      }
+    }
+    return dist
+}
+
+// Driver Code ------------
+
+// the input graph is an adjacency list 
+let inputGraph = {
+  'A': [['B', 2], ['C', 3], ['D', 6]],
+  'B': [['A', 2], ['D', 1], ['C', 1]],
+  'C': [['A', 3], ['D', 4], ['B', 1]],
+  'D': [['A', 6], ['B', 1], ['C', 4]],
+};
+
+console.log(dijikstras(inputGraph, "A"))
